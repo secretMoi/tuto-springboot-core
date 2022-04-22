bd7049360de636168e133efe7e2812f495073bc7
quentin.bolsee <quentin.bolsee@lampiris.be>
2022-04-22 10:22:22 +0200

    Les classes de configuration plus en détail

--- src/main/java/com/mycompany/invoise/AppConfig.java
+++ src/main/java/com/mycompany/invoise/AppConfig.java
@@ -1,18 +1,9 @@
 package com.mycompany.invoise;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.context.annotation.PropertySource;
 
-@Configuration // fichier de config
-@ComponentScan(basePackages = { // définit les implémentations à ustiliser
-        "com.mycompany.invoise.controller.web",
-        "com.mycompany.invoise.service.prefix",
-        "com.mycompany.invoise.repository.memory",
-})
-@PropertySource("application.properties") // charge le fichier de properties
-@ImportResource("classpath:applicationContext.xml") // charge la config du fichier XML
+
 public class AppConfig {
 
 }
