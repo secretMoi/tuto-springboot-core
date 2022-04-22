252f92a26f247643384cdf0e0c0bc12bc6c6b3b6
secretMoi <win.moi@hotmail.com>
2022-04-22 10:22:22 +0200

    Les classes de configuration plus en détail

--- src/main/java/com/mycompany/invoise/App.java
+++ src/main/java/com/mycompany/invoise/App.java
@@ -1,20 +1,41 @@
 package com.mycompany.invoise;
 
 import com.mycompany.invoise.controller.IInvoiceController;
+import com.mycompany.invoise.service.IInvoiceService;
+import com.mycompany.invoise.service.prefix.InvoiceServicePrefix;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 
+@Configuration // fichier de config
+@ImportResource("classpath:applicationContext.xml") // charge la config du fichier XML
+@ComponentScan(basePackages = { // définit les implémentations à ustiliser
+        "com.mycompany.invoise.controller.web",
+        //"com.mycompany.invoise.service.prefix",
+        "com.mycompany.invoise.repository.memory",
+})
+@PropertySource("application.properties") // charge le fichier de properties
 public class App 
 {
     public static void main( String[] args )
     {
 
 //        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+//        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class, AppConfigPropertySource.class);
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(App.class);
         var invoiceController = applicationContext.getBean(IInvoiceController.class);
 
 
         invoiceController.createInvoice();
 
+    }
+
+    @Bean // override l'injection de dépendance auto
+    public IInvoiceService configureInvoiceService() {
+        return new InvoiceServicePrefix();
     }
 }
---
+++ src/main/java/com/mycompany/invoise/AppConfigPropertySource.java
@@ -0,0 +1,8 @@
+package com.mycompany.invoise;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+
+
+public class AppConfigPropertySource {
+}
